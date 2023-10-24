44a500213cb935c2ebd2722065e628865a0a35ee
Torben Kneesch <Kneesch@gmx.de>
2023-10-24 20:19:27 +0200

    Switch movement to acceleration

--- PlayerShip.cs
+++ PlayerShip.cs
@@ -4,9 +4,14 @@ using System;
 public partial class PlayerShip : Area2D
 {
 	[Export]
-	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+	public float Acceleration { get; set; } = 200f; // How fast the player will move (pixels/sec).
+	
+	[Export]
+	public float RadsPerSecond { get; set; } = 2f * (float)Math.PI * 0.4f;
 	
 	public Vector2 ScreenSize;
+	
+	private Vector2 _velocity = Vector2.Zero;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -18,29 +23,32 @@ public partial class PlayerShip : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
 		var rotation = Rotation;
 
 		if (Input.IsActionPressed("turn_left"))
 		{
-			Rotation -= 10f * (float)delta;
+			Rotation -= RadsPerSecond * (float)delta;
 			Rotation = Rotation % (float)(2d * Math.PI);
 			GD.Print(Rotation);
 		}
 
 		if (Input.IsActionPressed("turn_right"))
 		{
-			Rotation += 10f * (float)delta;
+			Rotation += RadsPerSecond * (float)delta;
 			Rotation = Rotation % (float)(2d * Math.PI);
-			GD.Print(Rotation);
 		}
 
 		if (Input.IsActionPressed("thrust_forward"))
 		{
-			var newX = (float)(Position.X + Math.Cos(Rotation - Math.PI / 2d));
-			var newY = (float)(Position.Y + Math.Sin(Rotation - Math.PI / 2d));
-			var newPosition = new Vector2(newX, newY);
-			Position = newPosition;
-		}	
+			var vx = (float)(_velocity.X + Math.Cos(Rotation - Math.PI / 2d) * Acceleration * delta);
+			var vy = (float)(_velocity.Y + Math.Sin(Rotation - Math.PI / 2d) * Acceleration * delta);
+			_velocity = new Vector2(vx, vy);
+			GD.Print(vy);
+		}
+			
+		var newX = (float)(Position.X + _velocity.X * delta + ScreenSize.X) % ScreenSize.X;
+		var newY = (float)(Position.Y + _velocity.Y * delta + ScreenSize.Y) % ScreenSize.Y;
+		var newPosition = new Vector2(newX, newY);
+		Position = newPosition;
 	}
 }

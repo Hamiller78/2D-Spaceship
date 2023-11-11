65702ea10936baae97b59116bd8c5d4db9c9a77e
Torben Kneesch <Kneesch@gmx.de>
2023-11-11 16:28:19 +0100

    Add turret aim and fire logic

--- Turret.cs
+++ Turret.cs
@@ -3,22 +3,80 @@ using System;
 
 public partial class Turret : Area2D
 {
+	[Export]
+	public PackedScene LaserShotScene { get; set; }
+
+	public float TurnRate { get; set; } = 2f * (float)Math.PI * 0.2f;
+	public float FireRange { get; set; } = 300f;
+	public float ViewRange { get; set; } = 600f; 
+	public float RechargeTime { get; set; } = 0.5f;
 	private Vector2 _targetPosition;
+	private float _targetRotation;
+	private float _rechargeTimeRemaining = 0f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_rechargeTimeRemaining = RechargeTime;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_rechargeTimeRemaining > 0f)
+		{
+			_rechargeTimeRemaining -= (float)delta;
+		}	
+
+		var targetDistance = _targetPosition.DistanceTo(Position);
+		if (targetDistance > ViewRange)
+		{
+			return;
+		}
+
+		var rotationDelta = _targetRotation - Rotation;
+		var rotationStep = TurnRate * (float)delta;
+
+		if (Math.Abs(rotationDelta) <= rotationStep)
+		{
+			Rotation = _targetRotation;
+		}
+		else
+		{
+			Rotation += Math.Sign(rotationDelta) * rotationStep;
+		}
+
+		if (targetDistance > FireRange)
+		{
+			return;
+		}
+
+		FirePrimary();
 	}
 
 	public void OnTargetPositionUpdated(Vector2 position)
 	{
 		_targetPosition = position;
-		var targetRotation = (float)Math.Atan2(position.Y - Position.Y, position.X - Position.X) + (float)(Math.PI / 2d);
-		Rotation = targetRotation;
+		_targetRotation = (float)Math.Atan2(position.Y - Position.Y, position.X - Position.X) + (float)(Math.PI / 2d);
+	}
+
+	private void FirePrimary()
+	{
+		if (_rechargeTimeRemaining > 0f)
+		{
+			return;
+		}
+
+		var newShot = LaserShotScene.Instantiate<LaserShot>();
+		newShot.Position = Position;
+		newShot.Rotation = Rotation;
+		newShot.Velocity =
+			new Vector2(
+			  	(float)Math.Cos(newShot.Rotation - Math.PI / 2d) * newShot.Speed,
+				(float)Math.Sin(newShot.Rotation - Math.PI / 2d) * newShot.Speed
+			);
+		GetParent().AddChild(newShot);
+
+		_rechargeTimeRemaining = RechargeTime;
 	}
 }

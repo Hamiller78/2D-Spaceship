f1e4ca233184980a32d988f21c66531a664316a7
Torben Kneesch <Kneesch@gmx.de>
2023-11-11 17:57:48 +0100

    Fix and refactor turret turning

--- Turret.cs
+++ Turret.cs
@@ -8,7 +8,7 @@ public partial class Turret : Area2D
 
 	public float TurnRate { get; set; } = 2f * (float)Math.PI * 0.2f;
 	public float FireRange { get; set; } = 300f;
-	public float ViewRange { get; set; } = 600f; 
+	public float ViewRange { get; set; } = 600f;
 	public float RechargeTime { get; set; } = 0.5f;
 	private Vector2 _targetPosition;
 	private float _targetRotation;
@@ -26,7 +26,7 @@ public partial class Turret : Area2D
 		if (_rechargeTimeRemaining > 0f)
 		{
 			_rechargeTimeRemaining -= (float)delta;
-		}	
+		}
 
 		var targetDistance = _targetPosition.DistanceTo(Position);
 		if (targetDistance > ViewRange)
@@ -34,17 +34,7 @@ public partial class Turret : Area2D
 			return;
 		}
 
-		var rotationDelta = _targetRotation - Rotation;
-		var rotationStep = TurnRate * (float)delta;
-
-		if (Math.Abs(rotationDelta) <= rotationStep)
-		{
-			Rotation = _targetRotation;
-		}
-		else
-		{
-			Rotation += Math.Sign(rotationDelta) * rotationStep;
-		}
+		TurnTurret(delta);
 
 		if (targetDistance > FireRange)
 		{
@@ -57,7 +47,31 @@ public partial class Turret : Area2D
 	public void OnTargetPositionUpdated(Vector2 position)
 	{
 		_targetPosition = position;
-		_targetRotation = (float)Math.Atan2(position.Y - Position.Y, position.X - Position.X) + (float)(Math.PI / 2d);
+		_targetRotation = (float)(Math.Atan2(position.Y - Position.Y, position.X - Position.X) + Math.PI / 2d);
+	}
+
+	private void TurnTurret(double delta)
+	{
+		var rotationDelta = _targetRotation - Rotation;
+		if (rotationDelta < -Math.PI)
+		{
+			rotationDelta += (float)(2d * Math.PI);
+		}
+		else if (rotationDelta > Math.PI)
+		{
+			rotationDelta -= (float)(2d * Math.PI);
+		}
+		GD.Print($"Rotation: {Rotation}, Target: {_targetRotation}, Delta: {rotationDelta}");
+		var rotationStep = TurnRate * (float)delta;
+
+		if (Math.Abs(rotationDelta) <= rotationStep)
+		{
+			Rotation = _targetRotation;
+		}
+		else
+		{
+			Rotation += Math.Sign(rotationDelta) * rotationStep;
+		}
 	}
 
 	private void FirePrimary()
